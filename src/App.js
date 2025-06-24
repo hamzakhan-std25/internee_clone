ed1090b976026265415f085ee8f2a84658a3f768
Hamza khan <hamzakhan.std25@gmail.com>
2025-06-24 14:18:35 +0500

    upload react files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Home from './components/home'
+
+import './app.css'
+
+export default function App() {
+
+
+  return (
+    <div className='root'>
+      <h1>
+        Internee.pk website clone
+      </h1>
+
+    </div>
+  )
+}
